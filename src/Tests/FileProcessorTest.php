9677152f1253f3a12e6eae754108ec01550d256e
Jesus Diez <jesus.diez@akamon.com>
2015-10-25 21:54:05 +0100

    Update project name and some refactors and cleaning

--- src/Tests/FileProcessorTest.php
+++ src/Tests/FileProcessorTest.php
@@ -2,14 +2,30 @@
 
 namespace Consigment\Tests;
 
-class RemesaFileProcessorTest extends \PHPUnit_Framework_TestCase
+use Consigment\FileProcessor;
+
+class FileProcessorTest extends \PHPUnit_Framework_TestCase
 {
     public function test_line_type_is_identified()
     {
         $aOperationLine = "0121-04-201545111079993487741253  50294909XXXXXXXX0846    VXDN20-04-201500312431256005ON 00000001000+00005000000002+0000000000998+0032724325901                                      EUR001407642293      0000000000000+                                  \n";
 
-        $type = RemesaFileProcessor::indentifyLineType($line);
+        $type = FileProcessor::indentifyLineType($aOperationLine);
 
-        $this->assert('operation', $type);
+        $this->assertEquals('operation', $type);
+    }
+
+    public function test_line_is_parsed()
+    {
+        $aOperationLine = "0121-04-201545111079993487741253  50294909XXXXXXXX0846    VXDN20-04-201500312431256005ON 00000001000+00005000000002+0000000000998+0032724325901                                      EUR001407642293      0000000000000+                                  \n";
+        $aLineStructure = [
+            'Tipo de registro' => [2, '01'],
+            'Valor'            => [10],
+            'Remesa'           => [10],
+            'Factura'          => [12],
+        ];
+        $parsed = FileProcessor::parseLine($aOperationLine, $aLineStructure);
+
+        $this->assertEquals('21-04-2015', $parsed['Valor']);
     }
 }
